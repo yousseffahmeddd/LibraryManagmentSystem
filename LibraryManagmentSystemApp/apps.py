e3d800233ae7ef9fc84c6445261576e87e852f78
yousseffahmeddd <youssefahmedsaid845@gmail.com>
2024-05-12 03:21:21 +0300

    initial

---
+++ LibraryManagmentSystemApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LibrarymanagmentsystemappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'LibraryManagmentSystemApp'
