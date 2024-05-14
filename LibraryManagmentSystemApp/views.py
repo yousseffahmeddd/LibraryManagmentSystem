1dab8dd8aecc6591172e733c14df751633740970
yousseffahmeddd <youssefahmedsaid845@gmail.com>
2024-05-14 16:26:19 +0300

    initial

--- LibraryManagmentSystemApp/views.py
+++ LibraryManagmentSystemApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib import admin
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def home(request):
+    return render(request, "index.html", context={})
+
+
+def shopping(request):
+    return HttpResponse("Welcome to shopping")
